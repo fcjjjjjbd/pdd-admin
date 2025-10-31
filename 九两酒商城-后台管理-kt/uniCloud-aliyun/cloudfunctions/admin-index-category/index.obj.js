3ee9f828c12440b6ba05fea27ee24048622514d1
fcjjjjjbd <162014680+fcjjjjjbd@users.noreply.github.com>
2025-11-01 07:44:53 +0800

    a

--- 九两酒商城-后台管理-kt/uniCloud-aliyun/cloudfunctions/admin-index-category/index.obj.js
+++ 九两酒商城-后台管理-kt/uniCloud-aliyun/cloudfunctions/admin-index-category/index.obj.js
@@ -11,6 +11,175 @@ module.exports = {
   },
 
   //新增商家类型
+  async twoadd(params = {}) {
+    try {
+      let {
+        id,
+        errCode,
+        errMsg
+      } = await dbJQL.collection("afenleilist").add(params);
+      if (errCode !== 0) return result({
+        errCode: 400,
+        errMsg: "error",
+        custom: errMsg
+      });
+      return result({
+        errCode: 0,
+        errMsg: "success",
+        data: {
+          id
+        }
+      })
+    } catch (err) {
+      return result({
+        errCode: 500,
+        errMsg: "bug",
+        custom: err
+      })
+    }
+  },
+  //获取商家类型列表
+  async twolist() {
+    try {
+      let {
+        errCode,
+        data
+      } = await dbJQL.collection("afenleilist")
+        .field("_id,  name,sort")
+        .orderBy("sort asc").get();
+      if (errCode !== 0) return result({
+        errCode: 400,
+        errMsg: "error",
+        custom: errMsg
+      });
+      return result({
+        errCode: 0,
+        errMsg: "success",
+        data
+      });
+    } catch (err) {
+      return result({
+        errCode: 500,
+        errMsg: "bug",
+        custom: err
+      })
+    }
+  },
+  //修改
+  async twoupdate({
+    _id,
+    ...rest
+  } = {}) {
+    try {
+      if (!_id) return result({
+        errCode: 400,
+        errMsg: "error",
+        custom: "_id不能为空"
+      });
+      if (!Object.keys(rest).length) return result({
+        errCode: 400,
+        errMsg: "error",
+        custom: "更新的内容不能为空"
+      });
+      let {
+        errCode,
+        errMsg,
+        updated
+      } = await dbJQL.collection("afenleilist").doc(_id).update({
+        ...rest
+      });
+      if (errCode !== 0) return result({
+        errCode: 400,
+        errMsg: "error",
+        custom: errMsg
+      });
+      return result({
+        errCode: 0,
+        errMsg: "success",
+        data: {
+          updated
+        }
+      });
+    } catch (err) {
+      return result({
+        errCode: 500,
+        errMsg: "bug",
+        custom: err
+      })
+    }
+  },
+  //删除
+  async tworemove({
+    _id
+  } = {}) {
+    try {
+      if (!_id) return result({
+        errCode: 400,
+        errMsg: "error",
+        custom: "_id不能为空"
+      });
+
+      let {
+        errCode,
+        errMsg,
+        deleted
+      } = await dbJQL.collection("afenleilist").doc(_id).remove();
+      if (errCode !== 0) return result({
+        errCode: 400,
+        errMsg: "error",
+        custom: errMsg
+      });
+      return result({
+        errCode: 0,
+        errMsg: "success",
+        data: {
+          deleted
+        }
+      });
+    } catch (err) {
+      return result({
+        errCode: 500,
+        errMsg: "bug",
+        custom: err
+      })
+    }
+  },
+  //一条记录
+  async twodetail({
+    _id
+  } = {}) {
+    try {
+      if (!_id) return result({
+        errCode: 400,
+        errMsg: "error",
+        custom: "_id不能为空"
+      });
+      let {
+        errCode,
+        data
+      } = await dbJQL.collection("afenleilist").doc(_id)
+        .field(`_id,sort,name,create_date`)
+        .get({
+          getOne: true
+        });
+      if (errCode !== 0) return result({
+        errCode: 400,
+        errMsg: "error"
+      });
+      return result({
+        errCode: 0,
+        errMsg: "success",
+        data
+      });
+    } catch (err) {
+      return result({
+        errCode: 500,
+        errMsg: "bug",
+        custom: err
+      })
+    }
+  }
+ //新增商家类型
   async add(params = {}) {
     try {
       let {
--- 九两酒商城-后台管理-kt/uni_modules/uni-config-center/uniCloud/cloudfunctions/common/uni-config-center/uni-pay/config.js
+++ 九两酒商城-后台管理-kt/uni_modules/uni-config-center/uniCloud/cloudfunctions/common/uni-config-center/uni-pay/config.js
@@ -1,12 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 module.exports = {
-  // 0,https://ds.211415.top/uni-pay-co
-  notifyUrl: {
-    // 测试环境服务空间-支付回调地址
-    'mp-809a65a0-175d-4ce6-b53d-81522f24f79b': 'https://ds.211415.top/uni-pay-co',
+  "notifyUrl": {
     // 线上环境服务空间-支付回调地址mp-809a65a0-175d-4ce6-b53d-81522f24f79b
-    'mp-809a65a0-175d-4ce6-b53d-81522f24f79b': 'https://ds.211415.top/uni-pay-co'
+    'mp-f18d6178-7240-42d3-98b7-73076158595e': 'https://fc-mp-f18d6178-7240-42d3-98b7-73076158595e.next.bspapp.com/uni-pay-co'
   },
   notifyKey: '63C7718417D45FB2CD7C50762EB56918', // 跨云函数通信时的加密密钥，建议手动改下，不要使用默认的密钥，长度保持在32位即可
   // 微信支付相关
@@ -14,17 +11,17 @@ module.exports = {
     enable: true, // 是否启用微信支付
     // 微信 - 小程序支付
     mp: {
-      appId: 'wx3f17cf93d3a4823a', // 小程序的appid
-      secret: '87aee1b07a2732f6bf3abe0dc159db5f', // 小程序的secret
-      mchId: '1725575056', // 商户id
-      key: '', // v2的api key
+      appId: 'wx8e6e164b2cf51f88', // 小程序的appid
+      secret: 'b1ce55ca32161501348e38eda5149fd5', // 小程序的secret
+      mchId: '1688601235', // 商户id
+      key: 'cowyyj3p0pipjfj9p18tcyr11i4mbkca', // v2的api key
       pfx: fs.readFileSync(__dirname + '/wxpay/apiclient_cert.p12'), // v2需要用到的证书
       v3Key: '3Z7bP9xT2kF5dJ8hL1sQ4mN6vR7wC2yQ', // v3的api key
       appCertPath: path.join(__dirname, 'wxpay/apiclient_cert.pem'), // v3需要用到的证书
       appPrivateKeyPath: path.join(__dirname, 'wxpay/apiclient_key.pem'), // v3需要用到的证书
       wxpayPublicKeyPath: path.join(__dirname,
         'wxpay/pub_key.pem'), // v3需要用到的证书 - 微信支付公钥证书（仅限开启了微信支付公钥的商户，若已开通微信支付平台证书的商户可无视此参数）
-      version: 3 // 启用支付的版本 2代表v2版本 3 代表v3版本
+      version: 2 // 启用支付的版本 2代表v2版本 3 代表v3版本
     },
     // 微信 - APP支付
     app: {
@@ -112,8 +109,8 @@ module.exports = {
     },
     // 支付宝 - H5支付配置（包含：网站二维码、手机H5，需申请支付宝当面付接口权限）
     native: {
-      appId: '', // 支付宝开放平台下应用的appid
-      privateKey: '', // 支付宝商户私钥
+      appId: '2021004108648743', // 支付宝开放平台下应用的appid
+      privateKey: 'MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDcFJf84MQxqxTFrefXusAdxMpH0f/Qm+oPu9F6Cs3oZndY9nhdNioPMsUX6Vi74SPl5JDxbMtolI7tA0WqOHgS8UG9LZmlQSp8cGjVq7SkgLWNnq7aur0eKf6NQk4KcmA9pYVG4jhjoSce+3TT3Njmv4XzfxIiwYnknY6oJtWBaOUVEdObw8Y2jeii0FaxHbTDYvfAgMuRBXOp5bFGSCrS6snjzldrwojphVzeQlr4JaMkp74/bWB+xhJh8y3ff+rrXPp400DuZEFlTauCDm/svpo42XPUhCC2ofZvyWc5TXEqbEKbPAfUMPb+gtla7KbSI2SXI4pWlqcRNt5wIoPlAgMBAAECggEAKxB8jScOPQnVDrlw3G3Wmfyf+F3HSPK5YkrGOrNSh19Fi0rvUo3+vOqaVot6qYjslVecZSF8zAIF1asGPsC9/jbnPjVmE0glICR+3xCyUF4/KG8cmABicTyWat3Gl5S58FYrNiueDSDDnvRlb++PIdfoRjVfPx3P9cQ+3fh7Xs7i0OxTHvgz97+SS1DfZgCuWMTYa8fwfSBYSmcCpMopsvgany90Te4QgZ/iNl7fdKRxvdJy9yrDomQnqL/HnjqIVTP+uNQMgtyyIhK8A5kF4BZQxLlfon520iaZnoC3bY57rb5yjIkCZWvz9lLg+8dyLx/podBHIO1UAZ74ijUuIQKBgQD1+TdB/P63ArY/P8vBk3ZBzetKi3UiexHt5upDawvMR7YgXA8XfWQfMmp+JQbt6vkWeIKMBQ8P6wO8gDmwrZVXGZI3EOauFKB7aSz4fzRGAH5reLnOUHqvauIcx7zReqjmX61msXZ/WIZf4RDliEagykeiVaiPAma91u1iN+GpNwKBgQDlDS2xLNY/l76MQ35hPFClE21ul5LXdR28KimkPQlLmL6IKsKIpahisdO6BvwReQ3CckACmt904tCq53woHUWirUs+cFU7CX25jYSM4EGezS9loHXMPuyrkwcAXJ+14QUi5vhYa4mBzpV7cP3dLBKAOuu2uQ7YfXXrzcp66ynZwwKBgQC/ZAU9EVBYsoGAQXGX09fmaAW24UBpbf2LcWvCvZ5We04Za8hKbMTlCWOXd9Qmd/kNJQPfU2he6Gzl1ZgOA2L6fOkqc7oqpd4haLsXOFNCTMwRb8ST5kcwCyiZYElpbdnSN4xDJb6gZA9at1NgZmMeZYbLp38al7G98WiQTg8GjQKBgHI0uPSfe5AntC2xtYD8FuXIIwdCckgVsCbTCwJGIK1d0pqIX4jMuIHFUuFtfB+mMTMjzkC+Qe7A7PRaCXDi74amS18tmYGfQN3U7yNi6bUpb5mHgVS+hzkhTRtkE4oXUX1OlsNXky9y9tuewiWWRP9E4aSw7vMI01Qovu289ZgLAoGAFNNIcpVOIs6SkISh69XSQIAtUf0hEh//x7E07T1MoYsAN8jnS+l64oNF/WReot3G6GQedtEupVfmU7HI0XcHeRBlL8n7hCq61RzhmQLqP/lhnhHBsyFzeQPhdjijgj6/uky9SXsYA4fQWycFbc78SPvZbNDtgj2C+QZ/7iaTuSo=', // 支付宝商户私钥
       appCertPath: path.join(__dirname, 'alipay/appCertPublicKey.crt'), // 支付宝商户公钥路径
       alipayPublicCertPath: path.join(__dirname, 'alipay/alipayCertPublicKey_RSA2.crt'), // 支付宝公钥路径
       alipayRootCertPath: path.join(__dirname, 'alipay/alipayRootCert.crt') // 支付宝根证书路径
