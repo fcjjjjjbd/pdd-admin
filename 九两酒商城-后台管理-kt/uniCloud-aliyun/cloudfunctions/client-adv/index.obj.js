a52956c7da182a42941437892c4dd0cd6bf1c3e4
fcjjjjjbd <162014680+fcjjjjjbd@users.noreply.github.com>
2025-11-03 04:22:18 +0800

    a

--- 九两酒商城-后台管理-kt/uniCloud-aliyun/cloudfunctions/client-adv/index.obj.js
+++ 九两酒商城-后台管理-kt/uniCloud-aliyun/cloudfunctions/client-adv/index.obj.js
@@ -1,227 +1,212 @@
 // 开源
-let dbJQL = uniCloud.databaseForJQL()
-let {
-  result
-} = require("utils");
+let dbJQL = uniCloud.databaseForJQL();
+let { result } = require("utils");
 module.exports = {
-  _before: function() { // 通用预处理器
+  _before: function () {
+    // 通用预处理器
     const clientInfo = this.getClientInfo();
     dbJQL = uniCloud.databaseForJQL({
-      clientInfo
-    })
+      clientInfo,
+    });
   },
-
 
   // 获取分类数组
   async list() {
     try {
-      let {
-        errCode,
-        errMsg,
-        count,
-        data
-      } = await dbJQL.collection("aopen-fenlei")
+      let { errCode, errMsg, count, data } = await dbJQL
+        .collection("aopen-fenlei")
         .where(` is_on_sale == true `)
         .orderBy("sort asc")
         .field(`_id,name`)
         .get({
-          getCount: true
+          getCount: true,
         });
-      if (errCode !== 0) return result({
-        errCode: 400,
-        errMsg: "error",
-        custom: errMsg
-      });
+      if (errCode !== 0)
+        return result({
+          errCode: 400,
+          errMsg: "error",
+          custom: errMsg,
+        });
       return result({
         errCode: 0,
         errMsg: "success",
         data,
-        total: count
+        total: count,
       });
     } catch (err) {
       return result({
         errCode: 500,
         errMsg: "bug",
-        custom: err
-      })
+        custom: err,
+      });
     }
   },
   // 分类下列表广告贾谊
   async categorylist({
     pageSize = 10,
     pageCurrent = 1,
-    category_id = ''
+    category_id = "",
   } = {}) {
     try {
       pageSize = Math.min(10, pageSize);
       pageCurrent = (pageCurrent - 1) * pageSize;
-      let {
-        data,
-        errCode
-      } = await dbJQL
-        .collection('pdd-adv')
-        .where(`category_id=='${category_id}'`)
-        .orderBy('createTime desc')
+      let { data, errCode } = await dbJQL
+        .collection("pdd-adv")
+        .where(`category_id=='${category_id}' && order_status != 0`)
+        .orderBy("like_count desc")
         .skip(pageCurrent)
         .limit(pageSize)
         .get();
 
-      if (errCode !== 0) return result({
-        errCode: 400,
-        errMsg: 'error',
-        custom: errMsg
-      });
-      return result({
-        errCode: 0,
-        errMsg: 'success',
-        data
-      });
-    } catch (err) {
-      return result({
-        errCode: 500,
-        errMsg: 'bug',
-        custom: err
-      });
-    }
-  },
-  // 我的列表   
-
-  async myopen(id = "") {
-    try {
-      let {
-        errCode,
-        errMsg,
-        count,
-        data
-      } = await dbJQL.collection("pdd-adv")
-        .where(` user_id == '${id}' `)
-        .orderBy("publish_date desc")
-        .get({
-          getCount: true
+      if (errCode !== 0)
+        return result({
+          errCode: 400,
+          errMsg: "error",
+          custom: errMsg,
         });
-      if (errCode !== 0) return result({
-        errCode: 400,
-        errMsg: "error",
-        custom: errMsg
-      });
       return result({
         errCode: 0,
         errMsg: "success",
         data,
-        total: count
       });
     } catch (err) {
       return result({
         errCode: 500,
         errMsg: "bug",
-        custom: err
-      })
+        custom: err,
+      });
+    }
+  },
+  // 我的列表
+
+  async myopen(id = "") {
+    try {
+      let { errCode, errMsg, count, data } = await dbJQL
+        .collection("pdd-adv")
+        .where(` user_id == '${id}' `)
+        .orderBy("publish_date desc")
+        .get({
+          getCount: true,
+        });
+      if (errCode !== 0)
+        return result({
+          errCode: 400,
+          errMsg: "error",
+          custom: errMsg,
+        });
+      return result({
+        errCode: 0,
+        errMsg: "success",
+        data,
+        total: count,
+      });
+    } catch (err) {
+      return result({
+        errCode: 500,
+        errMsg: "bug",
+        custom: err,
+      });
     }
   },
   // 删除一个技术文章
   async remove(id) {
     const dbJQL = uniCloud.databaseForJQL({
-      clientInfo: this.getClientInfo()
+      clientInfo: this.getClientInfo(),
     });
     return await dbJQL.collection("pdd-adv").doc(id).remove();
   },
   //获取修改详情文章
   async detailxg(id = "") {
     try {
-      if (!id) return result({
-        errCode: 400,
-        errMsg: "error",
-        custom: "id不能为空"
+      if (!id)
+        return result({
+          errCode: 400,
+          errMsg: "error",
+          custom: "id不能为空",
+        });
+      let { data, errCode } = await dbJQL.collection("pdd-adv").doc(id).get({
+        getOne: true,
       });
-      let {
-        data,
-        errCode
-      } = await dbJQL.collection("pdd-adv").doc(id).get({
-        getOne: true
-      });
-      if (errCode !== 0) return result({
-        errCode: 400,
-        errMsg: "error",
-        custom: errMsg
-      });
+      if (errCode !== 0)
+        return result({
+          errCode: 400,
+          errMsg: "error",
+          custom: errMsg,
+        });
       return result({
         errCode: 0,
         errMsg: "success",
-        data
+        data,
       });
     } catch (err) {
       return result({
         errCode: 500,
         errMsg: "bug",
-        custom: err
-      })
+        custom: err,
+      });
     }
   },
   async add(params = {}) {
     try {
-      let {
-        errCode,
-        errMsg,
-        id
-      } = await dbJQL.collection("pdd-adv").add(params)
-      if (errCode !== 0) return result({
-        errCode: 400,
-        errMsg: "error",
-        custom: errMsg
-      });
+      let { errCode, errMsg, id } = await dbJQL
+        .collection("pdd-adv")
+        .add(params);
+      if (errCode !== 0)
+        return result({
+          errCode: 400,
+          errMsg: "error",
+          custom: errMsg,
+        });
       return result({
         errCode: 0,
         errMsg: "success",
         data: {
-          id
-        }
+          id,
+        },
       });
     } catch (err) {
       return result({
         errCode: 500,
         errMsg: "bug",
-        custom: err
-      })
+        custom: err,
+      });
     }
   },
   //修改
   async update(params = {}) {
     try {
-      let {
-        _id,
-        ...rest
-      } = params
-      if (!_id) return result({
-        errCode: 400,
-        errMsg: "error",
-        custom: "_id不能为空"
-      });
-      let {
-        errCode,
-        errMsg,
-        updated
-      } = await dbJQL.collection("pdd-adv").doc(_id).update({
-        ...rest
-      })
-      if (errCode !== 0) return result({
-        errCode: 400,
-        errMsg: "error",
-        custom: errMsg
-      });
+      let { _id, ...rest } = params;
+      if (!_id)
+        return result({
+          errCode: 400,
+          errMsg: "error",
+          custom: "_id不能为空",
+        });
+      let { errCode, errMsg, updated } = await dbJQL
+        .collection("pdd-adv")
+        .doc(_id)
+        .update({
+          ...rest,
+        });
+      if (errCode !== 0)
+        return result({
+          errCode: 400,
+          errMsg: "error",
+          custom: errMsg,
+        });
       return result({
         errCode: 0,
         errMsg: "success",
         data: {
-          updated
-        }
+          updated,
+        },
       });
     } catch (err) {
       return result({
         errCode: 500,
         errMsg: "bug",
-        custom: err
-      })
+        custom: err,
+      });
     }
-  }
-
-}
+  },
+};
--- 九两酒商城-后台管理-kt/uni_modules/uni-pay/uniCloud/cloudfunctions/uni-pay-co/notify/goods.js
+++ 九两酒商城-后台管理-kt/uni_modules/uni-pay/uniCloud/cloudfunctions/uni-pay-co/notify/goods.js
@@ -1,20 +1,17 @@
-const db = uniCloud.database();
-
+const db = uniCloud.database(); // 连接云对象整体
 module.exports = async (obj) => {
-	let user_order_success = true;
-	let { data = {} } = obj;
-	let { order_no, out_trade_no, total_fee, openid } = data;
-
-	let {
-		data: [payOrder],
-		errCode
-	} = await db.collection('JLJ-pay-order').where({ order_no }).field({ delivery_type: true, total_fee: true, order_status: true, goods_list: true }).get();
-	let statusValue = payOrder.delivery_type == 0 ? 2 : 1;
-	db.collection('JLJ-pay-order').where({ order_no }).update({
-		order_status: statusValue,
-		openid,
-		out_trade_no,
-		pay_date: Date.now()
-	});
-	return user_order_success;
+  let user_order_success = true;
+  let {
+    data = {}
+  } = obj;
+  let {
+    order_no,
+    out_trade_no,
+    total_fee
+  } = data; // uni-pay-orders 表内的数据均可获取到
+  await db.collection("pdd-adv").where({
+    order_no
+  }).update({
+    order_status: 1
+  })
 };
